8b20029e3717dc3744374d7e80b1dcf17bc9a4a5
qoomei <qoo12.mei03@gmail.com>
2022-08-22 22:14:40 +0900

    スクロール

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import classNames from 'classnames'
 import { useState } from 'react'
 import './style.min.css'
 
+const setVh = () => {
+  const vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+}
+window.addEventListener('load', setVh)
+window.addEventListener('resize', setVh)
+
 function App() {
   const [status, setStatus] = useState('progress')
   const statusClassNames = {
@@ -12,8 +19,47 @@ function App() {
 
   return (
     <div className="App">
-      <div className="container"></div>
-      <div className={classNames(statusClassNames)}></div>
+      <div className="container-fluid fixed-top d-flex align-items-center header">.X.X.</div>
+      <div className="container fixed-top main">
+        <div className={classNames(statusClassNames)}></div>
+        <p>A</p>
+        <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>X</p>
+        <p>Y</p>
+        <p>Z</p>
+      </div>
+      <div className="container-fluid fixed-bottom d-flex align-items-center footer">.X.X.</div>
     </div>
   )
 }
